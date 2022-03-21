6c771606a67000f122bcfa57ee15997bd3d6e7a5
sahil-sigmoid <sahils@sigmoidanalytics.com>
2022-03-21 11:31:20 +0530

    docker assignment  2

--- dags/Assignment.py
+++ dags/Assignment.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from create_table import sql_create_table
-from insert_table import insert_into_sql_table
 
 default_args = {
     "owner": "Sahil",
@@ -18,6 +17,5 @@ default_args = {
 dag = DAG("Assignment", default_args=default_args, schedule_interval="0 6 * * *")
 
 t1 = PythonOperator(task_id='create_sql_table', python_callable=sql_create_table, dag=dag)
-t2 = PythonOperator(task_id='insert_sql_table', python_callable=insert_into_sql_table, dag=dag)
 
-t1 >> t2
+t1
--- dags/create_table.py
+++ dags/create_table.py
@@ -1,29 +1,24 @@
+# import pandas
 import psycopg2
-import pandas as pd
 
 
 def sql_create_table():
+
     try:
-        create_query = """CREATE TABLE class(
-                FIRST_NAME VARCHAR(30),
-                LAST_NAME VARCHAR(30),
-                NUMBER NUMERIC(10),
-                GENDER VARCHAR(1))"""
+        conn = psycopg2.connect(host="postgres", database="airflow", user="airflow", password="airflow", port='5432')
+        cursor = conn.cursor()
 
-        connection = psycopg2.connect(host="postgres", database="airflow", user="airflow", password="airflow",
-                                      port='5432')
+        add_data = 'create table docker_sahil_table as select dag_id, execution_date from dag_run order by ' \
+                   'execution_date; '
+        cursor.execute(add_data)
+        conn.commit()
 
-        print("Database successfully connected.")
-        cur = connection.cursor()
-        print("Cursor defined")
-        cur.execute(create_query)
-        print("Successfully created table.")
+        print("data added to a new table Successfully")
+
+
 
     except:
-        print("Could not create table. Unknown error occurred")
-
+        print("Error in connection")
     finally:
-        if connection is not None:
-            connection.commit()
-            connection.close()
-
+        conn.close()
+        print("No issues")
