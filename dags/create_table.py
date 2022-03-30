59d85795bbe14e939c4fb649b5aa079f81bc3dd5
sahil-sigmoid <sahils@sigmoidanalytics.com>
2022-03-30 15:58:11 +0530

    docker assignment 2

--- dags/create_table.py
+++ dags/create_table.py
@@ -4,21 +4,11 @@ import psycopg2
 
 def sql_create_table():
 
-    try:
         conn = psycopg2.connect(host="postgres", database="airflow", user="airflow", password="airflow", port='5432')
         cursor = conn.cursor()
 
-        add_data = 'create table docker_sahil_table as select dag_id, execution_date from dag_run order by ' \
-                   'execution_date; '
+        add_data = 'create table IF NOT EXISTS docker_sahil_table as select dag_id, execution_date from dag_run; '
         cursor.execute(add_data)
         conn.commit()
 
         print("data added to a new table Successfully")
-
-
-
-    except:
-        print("Error in connection")
-    finally:
-        conn.close()
-        print("No issues")
